51ace4ec77f058ce9a0b20e41a871851a73c8821
ConnorDBurge <connor.d.burge@gmail.com>
2022-02-08 11:04:26 -0800

    CRUD Migrated to Func Rest Framework; Added Many-toMany relationship management between the Course and Student Models

--- school/urls.py
+++ school/urls.py
@@ -2,17 +2,28 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #
+    # API overview of all routes
     path('', views.api_overview, name='api_overview'),
     
+    #
+    # Student Model Routes
     path('students/list/', views.student_list, name='student_list'),
     path('students/detail/<int:student_id>/', views.student_detail, name='student_detail'),
     path('students/create/', views.student_create, name='student_create'),
     path('students/update/<int:student_id>/', views.student_update, name='student_update'),
     path('students/delete/<int:student_id>/', views.student_delete, name='student_delete'),
     
+    #
+    # Course Model Routes
     path('courses/list/', views.course_list, name='course_list'),
-    path('courses/detail/<int:courses_id>/', views.course_detail, name='course_detail'),
+    path('courses/detail/<int:course_id>/', views.course_detail, name='course_detail'),
     path('courses/create/', views.course_create, name='course_create'),
-    path('courses/update/<int:courses_id>/', views.course_update, name='course_update'),
-    path('courses/delete/<int:courses_id>/', views.course_delete, name='course_delete'),
+    path('courses/update/<int:course_id>/', views.course_update, name='course_update'),
+    path('courses/delete/<int:course_id>/', views.course_delete, name='course_delete'),
+    
+    #
+    # Many To Many Relationship Routes Between the Course and Student Models
+    path('students/enroll/<int:student_id>/<int:course_id>/', views.enroll_course, name='enroll_course'),
+    path('students/drop/<int:student_id>/<int:course_id>/', views.drop_course, name='enroll_course'),
 ]
--- school/views.py
+++ school/views.py
@@ -6,31 +6,36 @@ from .models import Student, Course
 @api_view(['GET'])
 def api_overview(request): # this method just returns an overview of the API routes
     api_urls = {
+        # Student Model Routes
         'Student List': 'students/list/',
         'Student Detail View': 'students/detail/<int:student_id>/',
         'Student Create': 'students/create/',
         'Student Update': 'students/update/<int:student_id>/',
         'Student Delete': 'students/delete/<int:student_id>',
+        # Course Model Routes
         'Course List': 'courses/list/',
-        'Course Detail View': 'courses/detail/<int:courses_id>/',
+        'Course Detail View': 'courses/detail/<int:course_id>/',
         'Course Create': 'courses/create/',
-        'Course Update': 'courses/update/<int:courses_id>/',
-        'Course Delete': 'courses/delete/<int:courses_id>'
+        'Course Update': 'courses/update/<int:course_id>/',
+        'Course Delete': 'courses/delete/<int:course_id>',
+        # Many-To-Many Relationship Routes Between the Course and Student Models
+        'Enroll Student in Course': 'students/enroll/<int:student_id>/<int:course_id>/',
+        'Drop Student from Course': 'students/drop/<int:student_id>/<int:course_id>/',
     }
     return Response(api_urls)
 
+#
+#
+#
+# Student Model Routes
+# List 
 @api_view(['GET'])
 def student_list(request):
     students = Student.objects.all()
     serializer = StudentSerializer(students, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
-def student_detail(request, student_id):
-    student = Student.objects.get(id=student_id)
-    serializer = StudentSerializer(student, many=False)
-    return Response(serializer.data)
-
+# Create
 @api_view(['POST'])
 def student_create(request):
     serializer = StudentSerializer(data=request.data)
@@ -38,6 +43,14 @@ def student_create(request):
         serializer.save()
     return Response(serializer.data)
 
+# Read
+@api_view(['GET'])
+def student_detail(request, student_id):
+    student = Student.objects.get(id=student_id)
+    serializer = StudentSerializer(student, many=False)
+    return Response(serializer.data)
+
+# Update
 @api_view(['POST'])
 def student_update(request, student_id):
     student = Student.objects.get(id=student_id)
@@ -46,24 +59,25 @@ def student_update(request, student_id):
         serializer.save()
     return Response(serializer.data)
 
+# Delete
 @api_view(['DELETE'])
 def student_delete(request, student_id):
     student = Student.objects.get(id=student_id)
     student.delete()
     return Response({'message': f'{student.first_name} {student.last_name} deleted'})
 
+#
+#
+#
+# Course Model Routes
+# List
 @api_view(['GET'])
 def course_list(request):
     courses = Course.objects.all()
     serializer = CourseSerializer(courses, many=True)
     return Response(serializer.data)
 
-@api_view(['GET'])
-def course_detail(request, course_id):
-    course = Course.objects.get(id=course_id)
-    serializer = CourseSerializer(course, many=False)
-    return Response(serializer.data)
-
+# Create
 @api_view(['POST'])
 def course_create(request):
     serializer = CourseSerializer(data=request.data)
@@ -71,6 +85,14 @@ def course_create(request):
         serializer.save()
     return Response(serializer.data)
 
+# Read
+@api_view(['GET'])
+def course_detail(request, course_id):
+    course = Course.objects.get(id=course_id)
+    serializer = CourseSerializer(course, many=False)
+    return Response(serializer.data)
+
+# Update
 @api_view(['POST'])
 def course_update(request, course_id):
     course = Course.objects.get(id=course_id)
@@ -79,8 +101,29 @@ def course_update(request, course_id):
         serializer.save()
     return Response(serializer.data)
 
+# Delete
 @api_view(['DELETE'])
 def course_delete(request, course_id):
     course = Course.objects.get(id=course_id)
     course.delete()
     return Response({'message': f'{course.first_name} {course.last_name} deleted'})
+
+#
+#
+#
+# Many To Many Relationship Routes Between the Course and Student Models
+# Create Relationship
+@api_view(['POST'])
+def enroll_course(request, student_id, course_id):
+    student = Student.objects.get(id=student_id)
+    course = Course.objects.get(id=course_id)
+    student.courses.add(course)
+    return Response({'message': f'{student.first_name} {student.last_name} enrolled in {course.course_id}'})
+
+# Drop Relationship
+@api_view(['POST'])
+def drop_course(request, student_id, course_id):
+    student = Student.objects.get(id=student_id)
+    course = Course.objects.get(id=course_id)
+    student.courses.remove(course)
+    return Response({'message': f'{student.first_name} {student.last_name} dropped from {course.course_id}'})
